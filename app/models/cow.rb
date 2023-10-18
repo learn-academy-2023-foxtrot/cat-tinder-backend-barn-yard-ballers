dae9c9b4628e37e1921993ce6b1141c8d50df164
LouisBrizuela <louisbrizz@gmail.com>
2023-10-18 11:31:56 -0700

    finished

--- app/models/cow.rb
+++ app/models/cow.rb
@@ -1,4 +1,7 @@
 class Cow < ApplicationRecord
     validates :name, presence: true
     validates :hobby, length: { minimum: 10 }
+    validates :breed, presence: true
+    validates :age, presence: true
+    validates :image, presence: true
 end
--- spec/models/cow_spec.rb
+++ spec/models/cow_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Cow, type: :model do
 
   it "must have a hobby of 10 characters" do
     cow = Cow.create( 
+    name: "Ottis",  
     breed: "Belgian Blue",
     age: 12,
     image: "https://livestockmiddleeast.com/wp-content/uploads/2019/06/KUBITUS-DE-BRAY-455716400-6047-e1561536471567.jpg"
@@ -20,4 +21,33 @@ RSpec.describe Cow, type: :model do
     expect(cow.errors[:hobby]).to_not be_empty
   end
 
+  it "must have an age" do
+    cow = Cow.create( 
+    name: "Ottis",  
+    breed: "Belgian Blue",
+    hobby: "Lift weights",
+    image: "https://livestockmiddleeast.com/wp-content/uploads/2019/06/KUBITUS-DE-BRAY-455716400-6047-e1561536471567.jpg"
+    )
+    expect(cow.errors[:age]).to_not be_empty
+  end
+
+  it "must have an image" do
+    cow = Cow.create( 
+    name: "Ottis",  
+    breed: "Belgian Blue",
+    age: 12,
+    hobby: "Lift weights"
+    )
+    expect(cow.errors[:image]).to_not be_empty
+  end
+
+  it "must have a breed" do
+    cow = Cow.create( 
+    name: "Ottis",  
+    age: 12,
+    hobby: "Lift weights",
+    image: "https://livestockmiddleeast.com/wp-content/uploads/2019/06/KUBITUS-DE-BRAY-455716400-6047-e1561536471567.jpg"
+    )
+    expect(cow.errors[:breed]).to_not be_empty
+  end
 end
